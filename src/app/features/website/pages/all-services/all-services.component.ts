c077bd72d7e079839dfd20d0aa44771e307b73ac
aboamin <eeaarrssnn40@gmail.com>
2025-11-22 00:29:47 +0200

    4th

--- src/app/features/website/pages/all-services/all-services.component.ts
+++ src/app/features/website/pages/all-services/all-services.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../../shared/header/header.component";
 import { FooterComponent } from "../../../../shared/footer/footer.component";
+import { DashboardRoutingModule } from "../../../dashboard/dashboard-routing.module";
 
 @Component({
   selector: 'app-all-services',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, DashboardRoutingModule],
   templateUrl: './all-services.component.html',
   styleUrl: './all-services.component.css'
 })
